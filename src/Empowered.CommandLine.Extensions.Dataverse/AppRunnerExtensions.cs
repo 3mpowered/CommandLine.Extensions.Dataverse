dd97afa5888233fd50278c3fd845f5dfb981559c
Simon Findling <simon.findling@outlook.de>
2024-09-04 02:29:40 +0200

    feat: add new middleware to test dataverse connection before command execution

---
+++ src/Empowered.CommandLine.Extensions.Dataverse/AppRunnerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using CommandDotNet;
+using CommandDotNet.Builders;
+using CommandDotNet.Execution;
+using Empowered.CommandLine.Extensions.Extensions;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Organization;
+using Microsoft.Xrm.Sdk.Query;
+using Spectre.Console;
+
+namespace Empowered.CommandLine.Extensions.Dataverse;
+
+public static class AppRunnerExtensions
+{
+    public static AppRunner UseDataverseConnectionTest<TOrganizationService>(this AppRunner appRunner)
+        where TOrganizationService : class, IOrganizationService
+    {
+        return appRunner.Configure(context =>
+            context.UseMiddleware(TestConnection<TOrganizationService>, MiddlewareStages.PostBindValuesPreInvoke)
+        );
+    }
+
+    private static Task<int> TestConnection<TOrganizationService>(CommandContext commandContext, ExecutionDelegate next)
+        where TOrganizationService : class, IOrganizationService
+    {
+        if (commandContext.DependencyResolver == null)
+        {
+            return next(commandContext);
+        }
+
+        var organizationService = commandContext.DependencyResolver.ResolveOrDefault<TOrganizationService>();
+        if (organizationService == null)
+        {
+            return next(commandContext);
+        }
+
+        var console = commandContext.DependencyResolver.ResolveOrDefault<IAnsiConsole>();
+        if (console == null)
+        {
+            return next(commandContext);
+        }
+
+        var whoAmI = (WhoAmIResponse)organizationService.Execute(new WhoAmIRequest());
+        var user = organizationService.Retrieve("systemuser", whoAmI.UserId, new ColumnSet("domainname"))
+            .GetAttributeValue<string>("domainname");
+        var currentOrganization =
+            (RetrieveCurrentOrganizationResponse)organizationService.Execute(new RetrieveCurrentOrganizationRequest
+                { AccessType = EndpointAccessType.Default });
+        console.Success(
+            $"Connected to environment {currentOrganization.Detail.Endpoints[EndpointType.WebApplication].Italic()} as {user.Italic()}");
+        return next(commandContext);
+    }
+}
---
+++ test/Empowered.CommandLine.Extensions.Dataverse.Tests/AppRunnerExtensionsTests.cs
@@ -0,0 +1,116 @@
+﻿using System.ServiceModel;
+using CommandDotNet;
+using FluentAssertions;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Organization;
+using Microsoft.Xrm.Sdk.Query;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Spectre.Console;
+
+namespace Empowered.CommandLine.Extensions.Dataverse.Tests;
+
+public class AppRunnerExtensionsTests
+{
+    private class TestCommand()
+    {
+        [DefaultCommand]
+        public async Task<int> Hello(IConsole console)
+        {
+            console.WriteLine("Hello World");
+            return await ExitCodes.Success;
+        }
+    }
+
+    [Fact]
+    public void ShouldTestConnectionBeforeCommandExecution()
+    {
+        var organizationService = Substitute.For<IOrganizationService>();
+        var systemUser = new Entity("systemuser", Guid.NewGuid())
+        {
+            ["domainname"] = "someone@example.com"
+        };
+        var organizationId = Guid.NewGuid();
+        organizationService
+            .Execute(Arg.Any<WhoAmIRequest>())
+            .Returns(new WhoAmIResponse
+            {
+                [nameof(WhoAmIResponse.OrganizationId)] = organizationId,
+                [nameof(WhoAmIResponse.BusinessUnitId)] = Guid.NewGuid(),
+                [nameof(WhoAmIResponse.UserId)] = systemUser.Id
+            });
+        const string webApplicationEndpoint = "https://localhost";
+        organizationService
+            .Execute(Arg.Any<RetrieveCurrentOrganizationRequest>())
+            .Returns(new RetrieveCurrentOrganizationResponse
+            {
+                [nameof(RetrieveCurrentOrganizationResponse.Detail)] = new OrganizationDetail
+                {
+                    OrganizationId = organizationId,
+                    Endpoints = new EndpointCollection
+                    {
+                        { EndpointType.WebApplication, webApplicationEndpoint }
+                    }
+                }
+            });
+        organizationService
+            .Retrieve(systemUser.LogicalName, systemUser.Id, Arg.Any<ColumnSet>())
+            .Returns(systemUser);
+        var appRunner = new EmpoweredAppRunner<TestCommand>("test",
+            (collection, _) => { collection.AddSingleton(organizationService); });
+        appRunner.UseDataverseConnectionTest<IOrganizationService>();
+
+        appRunner.Run().Should().Be(0);
+        organizationService
+            .Received()
+            .Execute(Arg.Any<WhoAmIRequest>());
+        organizationService
+            .Received()
+            .Execute(Arg.Any<RetrieveCurrentOrganizationRequest>());
+        organizationService
+            .Received()
+            .Retrieve(systemUser.LogicalName, systemUser.Id, Arg.Any<ColumnSet>());
+    }
+
+    [Fact]
+    public void ShouldSkipExecutionOnMissingDependencyResolver()
+    {
+        var appRunner = new AppRunner(typeof(TestCommand))
+            .UseDataverseConnectionTest<IOrganizationService>();
+
+        appRunner.Run().Should().Be(0);
+    }
+
+    [Fact]
+    public void ShouldSkipExecutionOnMissingOrganizationService()
+    {
+        var appRunner = new EmpoweredAppRunner<TestCommand>("test")
+            .UseDataverseConnectionTest<IOrganizationService>();
+
+        appRunner.Run().Should().Be(0);
+    }
+
+    [Fact]
+    public void ShouldFailOnThrowingOrganizationService()
+    {
+        var organizationService = Substitute.For<IOrganizationService>();
+        organizationService.Execute(Arg.Any<OrganizationRequest>())
+            .Throws(new FaultException<OrganizationServiceFault>(new OrganizationServiceFault
+                {
+                    Message = "No!"
+                },
+                new FaultReason("reason!")));
+        var appRunner = new EmpoweredAppRunner<TestCommand>("test",
+                (collection, builder) =>
+                {
+                    collection.Remove(new ServiceDescriptor(typeof(IAnsiConsole), typeof(AnsiConsole),
+                        ServiceLifetime.Singleton));
+                    collection.AddSingleton(organizationService);
+                })
+            .UseDataverseConnectionTest<IOrganizationService>();
+
+        appRunner.Run().Should().Be(1);
+    }
+}
